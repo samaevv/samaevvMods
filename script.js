8cdb6dddf02d1a4bc8ab779fb23f1c8efa447004
samaevv <artemsamaev25@gmail.com>
2026-09-10 20:10:33 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector('.menu67')
+const menuBtn = document.querySelector('.menuBtn')
+
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu67')
+});
+const btn = document.querySelector('.mod1').querySelector('.btn2')
+
+btn.addEventListener('click', ()=>{
+    window.open('index3.html', '_blank')
+})
+
+
+
+
+
